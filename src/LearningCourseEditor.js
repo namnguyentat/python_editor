95fac6d11ecd06a26755b76e9fb62e50d2b0fc78
namnguyentat <nguyentatnam53ac@gmail.com>
2019-09-18 09:14:08 +0700

    Update style and add run button

--- src/LearningCourseEditor.js
+++ src/LearningCourseEditor.js
@@ -11,6 +11,58 @@ import {
 } from 'monaco-languageclient';
 import normalizeUrl from 'normalize-url';
 import ReconnectingWebSocket from 'reconnecting-websocket';
+import * as thebelab from './thebelab';
+import $ from 'jquery';
+
+const printVarListCode = `import json
+from sys import getsizeof
+
+from IPython import get_ipython
+from IPython.core.magics.namespace import NamespaceMagics
+
+_nms = NamespaceMagics()
+_Jupyter = get_ipython()
+_nms.shell = _Jupyter.kernel.shell
+
+try:
+    import numpy as np  # noqa: F401
+except ImportError:
+    pass
+
+
+def _getsizeof(x):
+    # return the size of variable x. Amended version of sys.getsizeof
+    # which also supports ndarray, Series and DataFrame
+    if type(x).__name__ in ['ndarray', 'Series']:
+        return x.nbytes
+    elif type(x).__name__ == 'DataFrame':
+        return x.memory_usage().sum()
+    else:
+        return getsizeof(x)
+
+
+def _getshapeof(x):
+    # returns the shape of x if it has one
+    # returns None otherwise - might want to return an empty string for an empty collum
+    try:
+        return x.shape
+    except AttributeError:  # x does not have a shape
+        return None
+
+
+def var_dic_list():
+    types_to_exclude = ['module', 'function', 'builtin_function_or_method',
+                        'instance', '_Feature', 'type', 'ufunc']
+    values = _nms.who_ls()
+    vardic = [{'varName': v, 'varType': type(eval(v)).__name__, 'varSize': str(_getsizeof(eval(v))), 'varShape': str(_getshapeof(eval(v))) if _getshapeof(eval(v)) else '', 'varContent': str(eval(v))[:200]}  # noqa
+
+    for v in values if (v not in ['_html', '_nms', 'NamespaceMagics', '_Jupyter']) & (type(eval(v)).__name__ not in types_to_exclude)]  # noqa
+    return json.dumps(vardic)
+
+
+# command to refresh the list of variables
+print(var_dic_list())
+`;
 
 const deafultCode = `# Type your code here
 import random
@@ -54,8 +106,16 @@ class LearningCourseEditor extends React.Component {
     this.parsedCode = this.parseCode(deafultCode);
     this.characters = this.parsedCode.map(line => line.text.split(''));
     this.state = {
-      code: this.parsedCode.map(line => line.text).join('\n')
+      code: this.parsedCode.map(line => line.text).join('\n'),
+      runMode: null,
+      stdOut: null,
+      varList: null
     };
+
+    this.codeBlocks = [];
+    this.startRunLinByLine = false;
+    this.isWaitingServer = false;
+    this.executingLineNumber = null;
     this.deltaDecorations = [];
     this.contentWidgets = [];
     window.learning = this;
@@ -151,6 +211,7 @@ class LearningCourseEditor extends React.Component {
     MonacoServices.install(editor);
     this.showDecorations();
     this.showContentWidgets();
+    thebelab.bootstrap();
   };
 
   onChange = (newValue, e) => {
@@ -397,7 +458,22 @@ class LearningCourseEditor extends React.Component {
         }
       };
     });
-    return [...wormLineDecorations];
+    const executingLines = [];
+    if (this.startRunLinByLine && this.executingLineNumber) {
+      executingLines.push({
+        range: new this.monaco.Range(
+          this.executingLineNumber,
+          1,
+          this.executingLineNumber,
+          1
+        ),
+        options: {
+          isWholeLine: true,
+          className: 'bg-kov-orange'
+        }
+      });
+    }
+    return [...wormLineDecorations, ...executingLines];
   };
 
   hideLineDecoration = () => {
@@ -407,13 +483,265 @@ class LearningCourseEditor extends React.Component {
     );
   };
 
+  getCodeBlocks = () => {
+    const code = this.state.code;
+    const lines = code.split('\n');
+    let buffer = [];
+    let codeBlocks = [];
+    codeBlocks.push({
+      lineNo: null,
+      code: 'from IPython.core.debugger import set_trace'
+    });
+    lines.forEach((line, index) => {
+      if (
+        line.trim().length === 0 ||
+        line.trim().startsWith('#') ||
+        line.trim().startsWith('%')
+      ) {
+        return;
+      }
+      const lineNoInfo = `# lineNo:${index + 1}`;
+      const setTrace = 'set_trace()';
+      if (buffer.length === 0) {
+        if (line.endsWith(':')) {
+          buffer.push({
+            lineNo: index + 1,
+            code: line + lineNoInfo
+          });
+        } else {
+          codeBlocks = codeBlocks.concat([
+            {
+              lineNo: null,
+              code: setTrace + lineNoInfo
+            },
+            {
+              lineNo: index + 1,
+              code: line + lineNoInfo
+            }
+          ]);
+        }
+      } else {
+        if (line.startsWith(' ')) {
+          if (!line.endsWith(':')) {
+            const spaceCount = line.search(/\S/);
+            buffer.push({
+              lineNo: null,
+              code: `${' '.repeat(spaceCount)}set_trace()${lineNoInfo}`
+            });
+          }
+          buffer.push({
+            lineNo: index + 1,
+            code: line + lineNoInfo
+          });
+        } else {
+          codeBlocks.push({
+            lineNo: buffer[0].lineNo,
+            code: buffer.map(b => b.code).join('\n')
+          });
+          buffer = [];
+          if (line.endsWith(':')) {
+            buffer.push({
+              lineNo: index + 1,
+              code: line + lineNoInfo
+            });
+          } else {
+            codeBlocks = codeBlocks.concat([
+              {
+                lineNo: null,
+                code: setTrace + lineNoInfo
+              },
+              {
+                lineNo: index + 1,
+                code: line + lineNoInfo
+              }
+            ]);
+          }
+        }
+      }
+      if (index === lines.length - 1 && buffer.length > 0) {
+        codeBlocks.push({
+          lineNo: buffer[0].lineNo,
+          code: buffer.map(b => b.code).join('\n')
+        });
+      }
+    });
+    console.log(codeBlocks.map(b => b.code).join('\n'));
+    return codeBlocks;
+  };
+
+  runCode = () => {
+    this.restartEditor();
+    $('#output-area').show();
+    const kernel = window.thebeKernel;
+    const outputArea = window.outputArea;
+    const code = this.state.code;
+    outputArea.future = kernel.requestExecute({ code: code });
+    outputArea.future.done.then(() => {
+      let future = kernel.requestExecute({ code: printVarListCode });
+      future.onIOPub = msg => {
+        if (
+          msg.content &&
+          msg.content.name &&
+          msg.content.text &&
+          msg.content.name === 'stdout' &&
+          msg.content.text.startsWith('[{"varName":')
+        ) {
+          this.setState({ varList: JSON.parse(msg.content.text) });
+        }
+      };
+    });
+  };
+
+  finish = () => {
+    const kernel = window.thebeKernel;
+    kernel.sendInputReply({ status: 'ok', value: 'q' });
+    this.restartEditor();
+  };
+
+  runCodeLineByLine = () => {
+    if (this.isWaitingServer) {
+      return;
+    }
+    $('#output-area').show();
+    const kernel = window.thebeKernel;
+    const outputArea = window.outputArea;
+    outputArea.model.clear();
+    if (!this.startRunLinByLine) {
+      this.restartEditor();
+      this.startRunLinByLine = true;
+      this.isWaitingServer = true;
+      this.codeBlocks = this.getCodeBlocks();
+      const code = this.codeBlocks.map(b => b.code).join('\n');
+      let printVarFuture = kernel.requestExecute({ code: printVarListCode });
+      printVarFuture.done.then(() => {
+        let future = kernel.requestExecute({ code: code });
+        future.onIOPub = msg => {
+          // IDLE status
+          if (
+            msg.msg_type === 'status' &&
+            msg.content.execution_state === 'idle'
+          ) {
+            this.hideLineDecoration();
+            this.startRunLinByLine = false;
+            this.isWaitingServer = false;
+            this.forceUpdate()
+            return;
+          }
+
+          // Execute Result
+          if (msg.msg_type === 'execute_result') {
+            this.isWaitingServer = false;
+            if (msg.content.data && msg.content.data['text/plain']) {
+              this.setState({ stdOut: msg.content.data['text/plain'] });
+            }
+            outputArea.model.fromJSON([
+              {
+                data: msg.content.data,
+                metadata: msg.content.metadata,
+                output_type: 'execute_result'
+              }
+            ]);
+            return;
+          }
+
+          // Display Data
+          if (msg.msg_type === 'display_data') {
+            outputArea.model.fromJSON([
+              {
+                data: msg.content.data,
+                metadata: msg.content.metadata,
+                output_type: 'display_data'
+              }
+            ]);
+            return;
+          }
+
+          // Stream
+          if (
+            msg.msg_type === 'stream' &&
+            msg.content.name === 'stdout' &&
+            msg.content.text
+          ) {
+            this.isWaitingServer = false;
+            if (
+              msg.content.text.startsWith('[{"varName":') ||
+              msg.content.text.startsWith('[]')
+            ) {
+              this.setState({ varList: JSON.parse(msg.content.text) });
+            } else {
+              // Get var list
+              kernel.sendInputReply({
+                status: 'ok',
+                value: 'print(var_dic_list())'
+              });
+              outputArea.model.fromJSON([
+                {
+                  name: 'stdout',
+                  output_type: 'stream',
+                  text: msg.content.text
+                }
+              ]);
+              this.getLineNoAndShowDecoration();
+            }
+          }
+        };
+      });
+    } else {
+      kernel.sendInputReply({ status: 'ok', value: 'c' });
+      this.isWaitingServer = true;
+    }
+  };
+
+  getLineNoAndShowDecoration = () => {
+    const text = $('#output-area').text();
+    const lines = text.split('\n');
+    let lineNo = null;
+    lines.forEach((line, index) => {
+      if (line.startsWith('--->') || line.startsWith('---->')) {
+        console.log('line', line);
+        lineNo = line.split('# lineNo:')[1];
+        lineNo = parseInt(lineNo);
+        if (!Number.isInteger(lineNo)) {
+          lineNo = null;
+        }
+        if (lineNo) {
+          this.executingLineNumber = lineNo;
+          this.showDecorations();
+        }
+      }
+      if (line.startsWith('> <ipython-input')) {
+        this.setState({ stdOut: lines.slice(0, index).join('\n') });
+      }
+    });
+  };
+
+  restartEditor = () => {
+    this.codeBlocks = [];
+    this.startRunLinByLine = false;
+    this.isWaitingServer = false;
+    this.hideLineDecoration();
+    this.setState({
+      stdOut: null,
+      varList: null
+    });
+    const outputArea = window.outputArea;
+    outputArea.model.clear();
+  };
+
+  restartKernel = () => {
+    const kernel = window.thebeKernel;
+    kernel.restart().then(thebelab.bootstrap);
+  };
+
   calculateHeight = () => {
     return this.defaultCode.split('\n').length * 19;
   };
 
   render() {
     const code = this.state.code;
+    const { stdOut, varList } = this.state;
     const options = {
+      readOnly: this.startRunLinByLine,
       selectOnLineNumbers: true,
       minimap: {
         enabled: true
@@ -422,16 +750,57 @@ class LearningCourseEditor extends React.Component {
       selectionHighlight: false
     };
     return (
-      <MonacoEditor
-        width="800"
-        height={this.calculateHeight()}
-        language="python"
-        theme="vs"
-        value={code}
-        options={options}
-        onChange={this.onChange}
-        editorDidMount={this.editorDidMount}
-      />
+      <div>
+        <MonacoEditor
+          width="800"
+          height={this.calculateHeight()}
+          language="python"
+          theme="vs"
+          value={code}
+          options={options}
+          onChange={this.onChange}
+          editorDidMount={this.editorDidMount}
+        />
+        <div className="mb-3 mt-3">
+          <button className="btn btn-primary" onClick={this.runCode}>
+            Run
+          </button>
+          <button className="btn btn-success" onClick={this.runCodeLineByLine}>
+            Run line
+          </button>
+          <button className="btn btn-success" onClick={this.finish}>
+            Finish
+          </button>
+          <button className="btn btn-danger" onClick={this.restartKernel}>
+            Restart Kernel
+          </button>
+        </div>
+        {stdOut && (
+          <div>
+            <h3>Output</h3>
+            <pre>{stdOut}</pre>
+          </div>
+        )}
+        {varList && (
+          <div>
+            <h3>Variable Inspector</h3>
+            <table className="table table-sm table-striped">
+              <tbody>
+                {varList.map((variable, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{variable.varName}</td>
+                      <td>{variable.varType}</td>
+                      <td>{variable.varContent}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        )}
+        <div id="output-area" />
+      </div>
     );
   }
 }
